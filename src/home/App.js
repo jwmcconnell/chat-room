e46cc3e29d348c9b75adca81b4eb4207ed182e64
Jackmc <jackmc1122@gmail.com>
2019-06-03 15:19:30 -0700

    Add an App component and append to the index page.

---
+++ src/home/App.js
@@ -0,0 +1,15 @@
+import Component from '../Component.js';
+
+class App extends Component {
+  renderTemplate() {
+    return /*html*/`
+        <div>
+            <main>
+            Place Holder Text
+            </main>
+        </div>
+    `;
+  }
+}
+
+export default App;
