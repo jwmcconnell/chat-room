e67108213c28d34bd9771218d59a4eee8dce0400
Jackmc <jackmc1122@gmail.com>
2019-06-03 15:36:23 -0700

    Redirect user to auth page if they are not logged in on index page.

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+const config = {
+  apiKey: 'AIzaSyCJVB70CcXzm9jopkxageRgKeo5cz-BUS4',
+  authDomain: 'chat-room-e5f2a.firebaseapp.com',
+  databaseURL: 'https://chat-room-e5f2a.firebaseio.com',
+  projectId: 'chat-room-e5f2a',
+  storageBucket: 'chat-room-e5f2a.appspot.com',
+  messagingSenderId: '974879822629',
+  appId: '1:974879822629:web:80ed9bf5cc3eeed4'
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
