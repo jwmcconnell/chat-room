6b6019c7c0679386e9ef1d9bcafc3522d236083f
Jackmc <jackmc1122@gmail.com>
2019-06-03 15:38:08 -0700

    Append Header to Auth page.

--- src/auth/AuthApp.js
+++ src/auth/AuthApp.js
@@ -1,6 +1,18 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
 
 class AuthApp extends Component {
+
+  render() {
+    const dom = this.renderDOM();
+
+    const header = new Header();
+
+    dom.prepend(header.render());
+
+    return dom;
+  }
+
   renderTemplate() {
     return /*html*/`
       <div>
