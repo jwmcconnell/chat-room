cc5a51fba976dfd0a7a50ca1c6b26102f5085ee1
Jackmc <jackmc1122@gmail.com>
2019-06-03 15:21:54 -0700

    Add a Header component and add to the App.

--- src/home/App.js
+++ src/home/App.js
@@ -1,6 +1,18 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
 
 class App extends Component {
+
+  render() {
+    const dom = this.renderDOM();
+
+    const header = new Header();
+
+    dom.prepend(header.render());
+
+    return dom;
+  }
+
   renderTemplate() {
     return /*html*/`
         <div>
