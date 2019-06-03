631bbe0ab3c7064ad2370ef8d8d96918cafc06d2
Jackmc <jackmc1122@gmail.com>
2019-06-03 15:29:23 -0700

    Add auth html page and add AuthApp component to it.

---
+++ src/auth/AuthApp.js
@@ -0,0 +1,13 @@
+import Component from '../Component.js';
+
+class AuthApp extends Component {
+  renderTemplate() {
+    return /*html*/`
+      <div>
+        <main>Auth Page</main>
+      </div>
+    `;
+  }
+}
+
+export default AuthApp;
