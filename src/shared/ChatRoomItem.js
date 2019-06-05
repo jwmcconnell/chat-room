c5a642026a65b02dea1cd41e65d89c0591baea64
Jackmc <jackmc1122@gmail.com>
2019-06-05 10:47:45 -0700

    Only allow user who created the room to remove it.

--- src/shared/ChatRoomItem.js
+++ src/shared/ChatRoomItem.js
@@ -1,6 +1,26 @@
 import Component from '../Component.js';
+import { auth, roomsRef } from '../services/firebase.js';
 
 class ChatRoomItem extends Component {
+
+  render() {
+    const dom = this.renderDOM();
+
+    const room = this.props.room;
+
+    const roomRef = roomsRef.child(room.key);
+
+    const removeRoom = dom.querySelector('.remove-item');
+
+    removeRoom.addEventListener('click', () => {
+      if(room.owner === auth.currentUser.uid) {
+        roomRef.remove();
+      }
+    });
+
+    return dom;
+  }
+
   renderTemplate() {
     const room = this.props.room;
     return /*html*/`
