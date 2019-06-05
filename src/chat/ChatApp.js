90981fba3d433990e93f2907966b811245504257
Jackmc <jackmc1122@gmail.com>
2019-06-05 14:56:55 -0700

    Show chat room title in the header.

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -1,12 +1,26 @@
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
 
+import QUERY from '../utils/QUERY.js';
+import { roomsRef } from '../services/firebase.js';
+
 class ChatApp extends Component {
 
   render() {
     const dom = this.renderDOM();
 
-    const header = new Header({ title: 'Chat' });
+    const header = new Header({ title: 'Chat Room:' });
+
+    const searchParams = QUERY.parse(window.location.search.slice(1));
+
+    console.log(searchParams);
+
+    const roomRef = roomsRef.child(searchParams.key);
+
+    roomRef.on('value', snapshot => {
+      const value = snapshot.val();
+      header.update({ title: `Chat Room: ${value.name}` });
+    });
 
     dom.prepend(header.render());
 
