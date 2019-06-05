70b3ff5cd5b0e580d02d2c6aa4274528370aa367
Jackmc <jackmc1122@gmail.com>
2019-06-05 10:37:33 -0700

    Add button in div to sit on top of each card for potential removal.

--- src/shared/ChatRoomItem.js
+++ src/shared/ChatRoomItem.js
@@ -4,11 +4,15 @@ class ChatRoomItem extends Component {
   renderTemplate() {
     const room = this.props.room;
     return /*html*/`
+    <div class="item-wrapper">
       <a href="./chat.html#key=${room.key}">
         <li>
           <h2>${room.name}</h2>
         </li>
       </a>
+      <button class="remove-item">X</button>
+    </div>
+     
     `;
   }
 }
