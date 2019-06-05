c4f82d5c9f09399fc780f87796d9e92547167492
Jackmc <jackmc1122@gmail.com>
2019-06-05 10:04:55 -0700

    Add AddChatRoom component which adds a node on firebase for the chatroom added.

--- src/home/App.js
+++ src/home/App.js
@@ -1,6 +1,7 @@
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
 import ChatRoomList from '../shared/ChatRoomList.js';
+import AddChatRoom from '../shared/AddChatRoom.js';
 
 class App extends Component {
 
@@ -11,10 +12,13 @@ class App extends Component {
 
     const chatRoomList = new ChatRoomList();
 
+    const addChatRoom = new AddChatRoom();
+
     const main = dom.querySelector('main');
 
     dom.prepend(header.render());
 
+    main.appendChild(addChatRoom.render());
     main.appendChild(chatRoomList.render());
 
     return dom;
---
+++ src/shared/AddChatRoom.js
@@ -0,0 +1,33 @@
+import Component from '../Component.js';
+import { auth, roomsRef } from '../services/firebase.js';
+
+class AddChatRoom extends Component {
+  render() {
+    const dom = this.renderDOM();
+
+    const input = dom.querySelector('.chat-room-name');
+
+    dom.addEventListener('submit', event => {
+      event.preventDefault();
+      const roomRef = roomsRef.push();
+      roomsRef.child(roomRef.key).set({
+        key: roomRef.key,
+        owner: auth.currentUser.uid,
+        name: input.value,
+      });
+    });
+
+    return dom;
+  }
+
+  renderTemplate() {
+    return /*html*/`
+      <form>
+        <input class="chat-room-name">
+        <button>Submit</button>
+      </form>
+    `;
+  }
+}
+
+export default AddChatRoom;
