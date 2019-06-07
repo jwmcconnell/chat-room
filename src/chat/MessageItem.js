2e996858a098807e9ed5ce2779c8ed5ed915c536
Jackmc <jackmc1122@gmail.com>
2019-06-07 08:24:39 -0700

    Change styling on user profile.

--- src/chat/MessageItem.js
+++ src/chat/MessageItem.js
@@ -4,9 +4,16 @@ class MessageList extends Component {
 
   renderTemplate() {
     const message = this.props.message;
+    const owner = message.owner;
+
+    const avatar = owner.photoURL ? owner.photoURL : './assets/profile.png';
+
     return /*html*/`
-      <li>
-        <span>${message.owner.displayName}:</span>
+      <li class="message">
+        <section class="user">
+          <img src="${avatar}" class="message-avatar">
+          <span>${owner.displayName}:</span>
+        </section>
         <span>${message.message}</span>
       </li>
     `;
